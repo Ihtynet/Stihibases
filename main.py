f4d9d4b7658044ee7f369fcd2463fc2f4b5bdedf
Владимир <50101573+Ihtynet@users.noreply.github.com>
2022-12-17 15:14:53 +0300

    123

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def get_links(adr):
+    resp_a = requests.get(adr)
+    alltext_a = resp_a.text
+    soup = BeautifulSoup(alltext_a, "html.parser")
+    res = soup.find_all("a")
+    return res
+
+##################################
+nomfile = 1
+f = open("output"+str(nomfile)+".txt", "w")
+
+mas_links_a_a = get_links("https://stihibase.ru/author/")
+kol_a = 0
+kol_s = 0
+
+mas_links_a = []
+for links_a1 in mas_links_a_a:
+    links_a = links_a1.get("href")
+    if "/author/" in links_a and links_a.count("/") == 4 and links_a not in mas_links_a:
+        kol_a += 1
+        mas_links_a.append(links_a)
+        print(kol_a, links_a)
+
+        mas_links_s_a = get_links("https://stihibase.ru" + links_a)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        for links_s1 in mas_links_s_a:
+            links_s = links_s1.get("href")
+            if links_s.count("/") == 5:
+                kol_s += 1
+                if kol_s % 500 == 0:
+                    nomfile += 1;
+                    f.close()
+                    f = open("output"+str(nomfile)+".txt", "w")
+                print(kol_s, links_s)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+                f.write(links_s + "\n")
+f.close()
+f = open("output.txt", "r")
+mas = f.readlines()
+print(len(mas))
